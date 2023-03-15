6c97f13a632a5ebe93bec7a41c7fe8702156578f
n4t3y <nathan.lunney@outlook.com>
2023-03-16 12:13:47 +1300

    Added use z min endstop

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1345,7 +1345,7 @@
  * The probe replaces the Z-MIN endstop and is used for Z homing.
  * (Automatically enables USE_PROBE_FOR_Z_HOMING.)
  */
-//#define Z_MIN_PROBE_USES_Z_MIN_ENDSTOP_PIN
+#define Z_MIN_PROBE_USES_Z_MIN_ENDSTOP_PIN // NL 16/3/23 for Klackender
 
 // Force the use of the probe for Z-axis homing
 #define USE_PROBE_FOR_Z_HOMING // NL 14/3/23 for Klackender
